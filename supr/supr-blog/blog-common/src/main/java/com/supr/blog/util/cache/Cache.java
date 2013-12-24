7e1676592c95ae5752a2609cf2169f3caff3fcf1
smile <461504978@qq.com>
2013-12-24 13:37:14 +0800

    jj

--- supr/supr-blog/blog-common/src/main/java/com/supr/blog/util/cache/Cache.java
+++ supr/supr-blog/blog-common/src/main/java/com/supr/blog/util/cache/Cache.java
@@ -19,14 +19,30 @@ public interface Cache {
 	 * @param value
 	 * @throws CacheException
 	 */
-	void add(String key,Object value) throws CacheException;
+	void add(String key,Object value);
+	
+	/**
+	 * 指定块新增缓存
+	 * @param region
+	 * @param key
+	 * @param value
+	 * @throws CacheException
+	 */
+	void add(String region,String key,Object value);
 	
 	/**
 	 * 删除缓存
 	 * @param key
 	 * @throws CacheException
 	 */
-	void delete(String key) throws CacheException;
+	void delete(String key);
+	
+	/**
+	 * 指定块删除缓存
+	 * @param key
+	 * @throws CacheException
+	 */
+	void delete(String region,String key);
 	
 	/**
 	 * 更新缓存
@@ -34,7 +50,15 @@ public interface Cache {
 	 * @param value
 	 * @throws CacheException
 	 */
-	void update(String key,Object value) throws CacheException;
+	Boolean update(String key,Object value);
+	
+	/**
+	 * 指定块更新缓存
+	 * @param key
+	 * @param value
+	 * @throws CacheException
+	 */
+	Boolean update(String region,String key,Object value);
 	
 	/**
 	 * 根据key查找缓存
@@ -42,32 +66,79 @@ public interface Cache {
 	 * @return
 	 * @throws CacheException
 	 */
-	Object get(String key) throws CacheException;
+	Object get(String key);
+	
+	/**
+	 * 指定块根据key查找缓存
+	 * @param key
+	 * @return
+	 * @throws CacheException
+	 */
+	Object get(String region,String key);
+	
+	/**
+	 * 如果没有则增加 有则更新
+	 * @param key
+	 * @param value
+	 * @return
+	 */
+	Boolean saveOrUpdate(String key, Object value);
+
+	/**
+	 * 指定块如果没有则增加 有则更新
+	 * @param region
+	 * @param key
+	 * @param value
+	 * @return
+	 */
+    Boolean saveOrUpdate(String region, String key, Object value);
 	
 	/**
 	 * 清空系统缓存
 	 * @throws CacheException
 	 */
-	void flushAll() throws CacheException;
+	void flushAll();
+	
+	/**
+	 * 清空块缓存
+	 * @throws CacheException
+	 */
+	void flushRegion();
+	
+	/**
+	 * 缓存版本号递增
+	 * @param region
+	 * @param key
+	 * @return
+	 */
+	public Long incrementAndGet(String region, Object key);
+
+	/**
+	 * 缓存版本号递增
+	 * @param region
+	 * @param key
+	 * @return
+	 */
+    public Long incrementAndGet(Object key);
 	
 	/**
 	 * 清空模块级别缓存
 	 * @param modulAll
 	 * @throws CacheException
 	 */
-	void flushModulAll(String modulAll) throws CacheException;
+	void flushModulAll(String modulAll);
 	
 	/**
 	 * 清空用户级别缓存
 	 * @param userKey
 	 * @throws CacheException
 	 */
-	void flushUserAll(String userKey) throws CacheException;
+	void flushUserAll(String userKey);
 	
 	/**
 	 * 清空用户指定模块级别缓存
 	 * @throws CacheException
 	 */
-	void flushUserModulAll(String userKey,Object modulKey) throws CacheException;
+	void flushUserModulAll(String userKey,String modulKey);
 	
 }
