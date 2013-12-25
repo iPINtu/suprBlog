2f11cc1e850e67c13ff00a0a39ec994844a223af
smile <461504978@qq.com>
2013-12-25 10:31:36 +0800

    hh

---
+++ supr/supr-blog/blog-common/src/main/java/com/supr/blog/util/cache/memcache/MemcacheImpl.java
@@ -0,0 +1,116 @@
+package com.supr.blog.util.cache.memcache;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import net.spy.memcached.MemcachedClient;
+
+import com.supr.blog.util.cache.Cache;
+
+public class MemcacheImpl implements Cache {
+
+	@Autowired
+	private MemcachedClient memcacheClient;
+	
+	@Override
+	public void add(String key, Object value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void add(String region, String key, Object value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(String key) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(String region, String key) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Boolean update(String key, Object value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean update(String region, String key, Object value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object get(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object get(String region, String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean saveOrUpdate(String key, Object value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean saveOrUpdate(String region, String key, Object value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void flushAll() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void flushRegion(String region) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void flushModulCache(String modulKey) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void flushUserCache(String userKey) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void flushUserModulCache(String userKey, String modulKey) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Long incrementAndGet(String region, Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Long incrementAndGet(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
